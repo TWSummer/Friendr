10b9df8af03f8cbacae963b64ba19799b86798d0
Theodore Summer <TWSummer@gmail.com>
2018-02-01 14:50:52 -0800

    Add ability to update gender in ProfileAside Component

--- frontend/components/profile/profile_aside.jsx
+++ frontend/components/profile/profile_aside.jsx
@@ -3,12 +3,72 @@ import React from 'react';
 class profileAside extends React.Component {
   constructor(props) {
     super(props);
+    this.state = this.props.profile;
+  }
+
+  setEdit(e) {
+    this.setState({edit: true});
+  }
+
+  cancelEdit(e) {
+    this.setState({edit: false, gender: this.props.profile.gender});
+  }
+
+  updateGender(e) {
+    this.setState({gender: e.target.value});
+  }
+
+  handleSubmit(e) {
+    this.props.updateProfile({
+      id: this.state.id,
+      user_id: this.state.user_id,
+      gender: this.state.gender
+    })
+      .then(
+        () => this.setState({edit: false})
+      );
   }
 
   render() {
+    let genderDisplayContents;
+    if (this.state.edit) {
+      genderDisplayContents = (
+        <form>
+          <select
+            className="gender-select"
+            value={this.state.gender}
+            onChange={this.updateGender.bind(this)}>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+            <option value="Prefer Not to Say">Prefer Not to Say</option>
+          </select>
+          <br/>
+          <button
+            onClick={this.handleSubmit.bind(this)}
+            className="submit-response-button"
+            >Save</button>
+          <button
+            onClick={this.cancelEdit.bind(this)}
+            className="cancel-response-button"
+            >Cancel</button>
+        </form>
+
+      );
+    } else {
+      genderDisplayContents = (
+        <div
+          className="gender-display"
+          onClick={this.setEdit.bind(this)}>
+          {this.state.gender}
+          <i className="fas fa-pencil-alt"></i>
+        </div>
+      );
+    }
     return (
       <aside className="profile-aside">
-        This is the aside
+        <b>Gender</b><br />
+        {genderDisplayContents}
       </aside>
     );
   }
