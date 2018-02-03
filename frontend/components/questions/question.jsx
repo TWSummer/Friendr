215663bb9016afe44d4ce52d95932e20547b04ef
Theodore Summer <TWSummer@gmail.com>
2018-02-03 09:34:55 -0800

    Finish styling questions page begin adding event handlers

--- frontend/components/questions/question.jsx
+++ frontend/components/questions/question.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { merge } from 'lodash';
 
 class Question extends React.Component {
   constructor(props) {
@@ -10,7 +11,8 @@ class Question extends React.Component {
         question: "",
         options: [],
         answerCount: 0
-      }
+      },
+      friendAnswer: {}
     };
   }
 
@@ -23,7 +25,47 @@ class Question extends React.Component {
     );
   }
 
+  setImportance(num) {
+    return ( (e) => {
+      this.setState({importance: num});
+    });
+  }
+
+  setMyAnswer(optionId) {
+    return ( (e) => {
+      if (e.target.value) {
+        this.setState({myAnswer: optionId});
+      }
+    });
+  }
+
+  setFriendAnswer(optionId) {
+    return ( (e) => {
+      if (e.target.value) {
+        let friendAnswer = merge({}, this.state.friendAnswer);
+        if (friendAnswer[optionId]) {
+          delete friendAnswer[optionId];
+        } else {
+          friendAnswer[optionId] = optionId;
+        }
+        this.setState({friendAnswer});
+      }
+    });
+  }
+
   render() {
+    console.log(this.state);
+    let highlightLow, highlightMed, highlightHigh;
+    if (this.state.importance === 1) {
+      highlightLow = "importance-highlight";
+    } else if (this.state.importance === 3) {
+      highlightLow = "importance-highlight";
+      highlightMed = "importance-highlight";
+    } else if (this.state.importance === 7) {
+      highlightLow = "importance-highlight";
+      highlightMed = "importance-highlight";
+      highlightHigh = "importance-highlight";
+    }
     return (
       <div className="question-page">
         <header className="question-page-header">
@@ -45,7 +87,11 @@ class Question extends React.Component {
               <form className="question-form">
                 {
                   this.state.question.options.map( (option, idx) => (
-                    <label key={option.id} className="radio-label">
+                    <label
+                      key={option.id}
+                      className="radio-label"
+                      onClick={this.setMyAnswer(option.id).bind(this)}
+                      >
                       <input
                         type="radio"
                         className="radio-button"
@@ -62,7 +108,11 @@ class Question extends React.Component {
                 </h4>
                 {
                   this.state.question.options.map( (option, idx) => (
-                    <label key={option.id} className="check-label">
+                    <label
+                      key={option.id}
+                      className="check-label"
+                      onClick={this.setFriendAnswer(option.id).bind(this)}
+                      >
                       <input
                         type="checkbox"
                         className="checkbox"
@@ -84,10 +134,25 @@ class Question extends React.Component {
                   <span className="checkmark"></span>
                   Any of the above<br/>
                 </label>
+                <h4>Importance</h4>
                 <div className="importance-selector">
-                  <div className={`importance-low`}></div>
-                  <div className={`importance-med`}></div>
-                  <div className={`importance-high`}></div>
+                  <div
+                    className={`importance-button ${highlightLow}`}
+                    onClick={this.setImportance(1).bind(this)}
+                    ></div>
+                  <div
+                    className={`importance-button ${highlightMed}`}
+                    onClick={this.setImportance(3).bind(this)}
+                    ></div>
+                  <div
+                    className={`importance-button ${highlightHigh}`}
+                    onClick={this.setImportance(7).bind(this)}
+                    ></div>
+                </div>
+                <div className="importance-labels">
+                  <p>Low</p>
+                  <p>Medium</p>
+                  <p>High</p>
                 </div>
                 <div className="answer-buttons">
                   <button className="answer-button">Answer</button>
@@ -97,9 +162,12 @@ class Question extends React.Component {
             </article>
           </section>
           <aside className="question-aside">
-            <div className="highest-percentage">
-              <h1>{this.state.question.answerCount < 1 ? "0" : Math.round(1000 * (1.0 - 1.0/this.state.question.answerCount))/10}</h1>
-              <h2>%</h2>
+            <div className="highest-percentage-outer-box">
+              <div className="highest-percentage">
+                <h1>{this.state.question.answerCount < 1 ? "0" : Math.round(1000 * (1.0 - 1.0/this.state.question.answerCount))/10}</h1>
+                <h2>%</h2>
+              </div>
+              <h4>Highest Compatibility Possible</h4>
             </div>
             <h3>
               You've answered {this.state.question.answerCount} questions
