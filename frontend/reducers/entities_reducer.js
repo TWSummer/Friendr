dfd56afd42c25c6722b48aec58b4c8a40ec088f5
Theodore Summer <TWSummer@gmail.com>
2018-02-07 17:05:46 -0800

    Add messages and message errors to store

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import profileReducer from './profile_reducer';
 import searchReducer from './search_reducer';
+import messageReducer from './message_reducer';
 
 const entitiesReducer = combineReducers({
   profiles: profileReducer,
-  search: searchReducer
+  search: searchReducer,
+  messages: messageReducer
 });
 
 export default entitiesReducer;
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import sessionErrorsReducer from './session_errors_reducer';
 import profileErrorsReducer from './profile_errors_reducer';
 import searchErrorsReducer from './search_errors_reducer';
+import messageErrorsReducer from './message_errors_reducer';
 
 export default combineReducers({
   session: sessionErrorsReducer,
   profile: profileErrorsReducer,
-  search: searchErrorsReducer
+  search: searchErrorsReducer,
+  message: messageErrorsReducer
 });
