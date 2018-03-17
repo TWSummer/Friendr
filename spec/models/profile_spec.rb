cd83eef910d7007815eb288cda92067f3aec0661
Theodore Summer <TWSummer@gmail.com>
2018-03-16 17:40:00 -0700

    Improved test validating length of name in profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
   it { should validate_presence_of(:name) }
-  it do
-    should validate_length_of(:name).
-      is_at_most(24).
-      on(:create)
-  end
+  it { should validate_length_of(:name).is_at_most(24) }
   it { should validate_presence_of(:birthdate) }
   it { should validate_presence_of(:gender) }
   it { should validate_inclusion_of(:gender).in_array(["Male", "Female", "Other", "Prefer Not to Say"])}
