bc137ca661f25d01e29c558a51bf34cc68363d18
Theodore Summer <TWSummer@gmail.com>
2018-03-16 17:23:40 -0700

    Add test to validation of min_age to Search Query

--- spec/models/search_query_spec.rb
+++ spec/models/search_query_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe SearchQuery, type: :model do
   describe("Associations") do
     it { should belong_to(:user) }
   end
+
+  describe("Validations") do
+    it "validates that min age is at least 18" do
+      query = SearchQuery.new(min_age: 17)
+      query.user = User.new
+      expect(query).not_to be_valid
+
+      query.min_age = 18
+      expect(query).to be_valid
+    end
+  end
 end
