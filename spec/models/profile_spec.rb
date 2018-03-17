eb5a2fcbb839fb1567244e0e1e086476461ff4df
Theodore Summer <TWSummer@gmail.com>
2018-03-16 17:42:06 -0700

    Improve code style of profile gender test

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe Profile, type: :model do
   it { should validate_length_of(:name).is_at_most(24) }
   it { should validate_presence_of(:birthdate) }
   it { should validate_presence_of(:gender) }
-  it { should validate_inclusion_of(:gender).in_array(["Male", "Female", "Other", "Prefer Not to Say"])}
+  it do
+    should validate_inclusion_of(:gender)
+      .in_array(["Male", "Female", "Other", "Prefer Not to Say"])
+  end
 end
